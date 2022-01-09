d6231547e52d29d05319696074d6da0ea101e108
David Middleton <middleton.davida@gmail.com>
2022-01-09 14:57:00 -0500

    createNoteDiv function added

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const textInputArea = document.getElementById("text-input");
+const textInputArea = document.getElementById('text-input');
+const submitButton = document.getElementById('submit-button');
+const noteArea = document.getElementById('note-area');
 
 function getInputText() {
     let inputText = textInputArea.value;
     return inputText;
+}
+
+function createNoteDiv() {
+    const noteDiv = document.createElement('div');
+    noteDiv.classList.add('note')
+    noteArea.appendChild(noteDiv);
 }
