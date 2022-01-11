06f46bef4f57f3087f0a080f42a90f6fc51621d9
David Middleton <middleton.davida@gmail.com>
2022-01-10 22:44:55 -0500

    added createNote function. Consolidated getInputText function inside createNote function.

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const textInputArea = document.getElementById('text-input');
 const submitButton = document.getElementById('submit-button');
 const noteArea = document.getElementById('note-area');
 
-function getInputText() {
-    let inputText = textInputArea.value;
-    return inputText;
+// function getInputText() {
+//     let inputText = textInputArea.value;
+//     return inputText;
+// }
+
+function createNote() {
+    const noteDiv = document.createElement('div');
+    const inputText = textInputArea.value;
+    noteDiv.classList.add('note')
+    noteDiv.appendChild(document.createTextNode(inputText));
+    noteArea.appendChild(noteDiv);
 }
 
-function createNoteDiv() {
-    const noteDiv = document.createElement('div');
-    noteDiv.classList.add('note')
-    noteArea.appendChild(noteDiv);
+function drawNote() {
+
 }
